6a7f379dc7cab9b1dce0eba8428d7d277896fe44
Tony Maluki <tonymaluki@gmail.com>
2022-06-01 01:42:10 +0300

    using postman

--- EXPRESS-DEMO/app.js
+++ EXPRESS-DEMO/app.js
@@ -1,5 +1,8 @@
+const Joi = require("joi");
+const { request } = require("express");
 const express = require("express");
 const app = express();
+app.use(express.json());
 const courses = [
   { id: 1, name: "Biology" },
   { id: 2, name: "Chemistry" },
@@ -17,13 +20,30 @@ app.get("/api/courses/:id", (req, res) => {
   if (!course) res.status(404).send("Course was not found");
   res.send(course);
 });
+app.post("/api/courses", (req, res) => {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+
+  // if (!req.body.name || request.body.name < 3) {
+  //   // 400 Bad Request
+  //   req.status(400).send("Name is required and minimum 3 characters");
+  //   return;
+  // }
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
 const port = process.env.PORT || 3000;
 // On terminal , i have set PORT = 5000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}.....`);
 });
 
-// OUTPUT ON BROWER:
+// OUTPUT ON BROWSER:
 // when you load url : 'localhost:5000/api/courses/1'  ---
 //   (cont..)  will return the course Object with id of 1 ie : Biology
 
