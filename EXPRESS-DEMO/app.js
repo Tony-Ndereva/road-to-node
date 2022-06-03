9baf2f5c292edfc48f7371be7029bdddf3e5eb5e
Tony Maluki <tonymaluki@gmail.com>
2022-06-03 16:33:30 +0300

    Lots of changes

--- EXPRESS-DEMO/app.js
+++ EXPRESS-DEMO/app.js
@@ -1,5 +1,4 @@
-const Joi = require("joi");
-const { request } = require("express");
+const { AuthSchema } = require("./validate_schema");
 const express = require("express");
 const app = express();
 app.use(express.json());
@@ -8,12 +7,6 @@ const courses = [
   { id: 2, name: "Chemistry" },
   { id: 3, name: "Physics" },
 ];
-app.get("/", (req, res) => {
-  res.send("Hello world!!");
-});
-app.get("/api/courses", (req, res) => {
-  res.send(courses);
-});
 
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
@@ -21,15 +14,11 @@ app.get("/api/courses/:id", (req, res) => {
   res.send(course);
 });
 app.post("/api/courses", (req, res) => {
-  const schema = {
-    name: Joi.string().min(3).required(),
-  };
-
-  // if (!req.body.name || request.body.name < 3) {
-  //   // 400 Bad Request
-  //   req.status(400).send("Name is required and minimum 3 characters");
-  //   return;
-  // }
+  const result = AuthSchema.validate(req.body);
+  if (result.error) {
+    res.status(404).send(result.error.details[0].message);
+    return;
+  }
   const course = {
     id: courses.length + 1,
     name: req.body.name,
@@ -37,6 +26,26 @@ app.post("/api/courses", (req, res) => {
   courses.push(course);
   res.send(course);
 });
+app.put("api/courses/:id", (req, res) => {
+  // Look up for the course
+  // If does not exist, return 404
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(400).send("Course was not found");
+  // Validate
+  // If invalid, return 400 - Bad request
+  const result = AuthSchema.validate(req.body);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+  }
+
+  //Update course
+
+  course.name = req.body.name;
+
+  // Return the updated course
+  res.send(course);
+});
+
 const port = process.env.PORT || 3000;
 // On terminal , i have set PORT = 5000;
 app.listen(port, () => {
