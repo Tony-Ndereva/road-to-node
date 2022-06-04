d27bc95b67f056e8a71d9d90ec7be8c68950a9b3
Tony Maluki <tonymaluki@gmail.com>
2022-06-04 15:29:43 +0300

    implemented user's data validation

--- VIDLY-APP/app.js
+++ VIDLY-APP/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+const { VidlySchema } = require("../EXPRESS-DEMO/validate_schema");
 
 const genres = [];
 
 app.post("/api/genres", (req, res) => {
+  const results = VidlySchema.validate(req.body);
+  if (results.error)
+    return res.status(404).send(results.error.details[0].message);
+
   const genre = {
     id: genres.length + 1,
     type: req.body.type,
@@ -17,18 +22,27 @@ app.post("/api/genres", (req, res) => {
 app.get("/api/genres", (req, res) => {
   res.send(genres);
 });
+
 app.get("/api/genres/:id", (req, res) => {
   const genre = genres.find((c) => c.id === parseInt(req.params.id));
   res.send(genres);
 });
+
 app.put("/api/genres/:id", (req, res) => {
   const genre = genres.find((c) => c.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send("The genre was not found!");
+
+  const results = VidlySchema.validate(req.body);
+  if (results.error)
+    return res.status(404).send(results.error.details[0].message);
   genre.type = req.body.type;
   res.send(genre);
 });
 
 app.delete("/api/genres/:id", (req, res) => {
   const genre = genres.find((c) => c.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send("The genre was not found!");
+
   const index = genres.indexOf(genre);
   genres.splice(index, 1);
   res.send("Deleted successfully");
