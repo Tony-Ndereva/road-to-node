0899504a2543281b33e59a73c5736605b0197803
Tony Maluki <tonymaluki@gmail.com>
2022-06-10 21:11:58 +0300

    re structuring node application

--- EXPRESS-DEMO/app.js
+++ EXPRESS-DEMO/app.js
@@ -6,6 +6,7 @@ const authenticator = require("./authenticator");
 const helmet = require("helmet"); // will Look at this
 const morgan = require("morgan"); // Will look at this
 const config = require("config"); // Will look at this
+const courses = require("./courses");
 
 // Will look at thesw
 const debug = require("debug")("app:startup");
@@ -16,6 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(helmet());
+
+app.use("/api/courses", courses);
 
 // Configuration
 
@@ -29,67 +32,7 @@ if (app.get("env") === "development") {
 
 app.use(logger);
 app.use(authenticator);
-const courses = [
-  { id: 1, name: "Biology" },
-  { id: 2, name: "Chemistry" },
-  { id: 3, name: "Physics" },
-];
 
-app.get("/", (req, res) => {
-  res.render("index", { title: "My Express App", message: "Hello" });
-});
-app.get("/api/courses", (req, res) => {
-  res.send(courses);
-});
-app.get("api/courses/:id", (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course) return res.status(404).send("Course was not found");
-  res.send(course);
-});
-app.post("/api/courses", (req, res) => {
-  const result = AuthSchema.validate(req.body);
-  if (result.error)
-    return res.status(404).send(result.error.details[0].message);
-
-  const course = {
-    id: courses.length + 1,
-    name: req.body.name,
-  };
-  courses.push(course);
-  res.send(course);
-});
-app.put("/api/courses/:id", (req, res) => {
-  // Look up for the course
-  // If does not exist, return 404
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course) return res.status(404).send("Course was not found");
-
-  // Validate
-  // If invalid, return 400 - Bad request
-  const result = AuthSchema.validate(req.body);
-  if (result.error)
-    return res.status(400).send(result.error.details[0].message);
-
-  //Update course
-
-  course.name = req.body.name;
-
-  // Return the updated course
-  res.send(course);
-});
-
-app.delete("/api/courses/:id", (req, res) => {
-  // Look for the course
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
-
-  if (!course) return res.status(404).send("Course is not available");
-
-  //Delete
-  const index = courses.indexOf(course);
-  courses.splice(index, 1);
-  for (let key in course)
-    res.send(`course ${key}  has been deleted successfully`);
-});
 
 const port = process.env.PORT || 3000;
 // On terminal , i have set PORT = 5000;
---
+++ EXPRESS-DEMO/routes/courses.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const router = express.Router();
+const { AuthSchema } = require("./validate_schema");
+
+
+
+
+
+const courses = [
+  { id: 1, name: "Biology" },
+  { id: 2, name: "Chemistry" },
+  { id: 3, name: "Physics" },
+];
+
+router.get("/", (req, res) => {
+  res.render("index", { title: "My Express App", message: "Hello" });
+});
+router.get("/", (req, res) => {
+  res.send(courses);
+});
+router.get("/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send("Course was not found");
+  res.send(course);
+});
+router.post("/", (req, res) => {
+  const result = AuthSchema.validate(req.body);
+  if (result.error)
+    return res.status(404).send(result.error.details[0].message);
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+router.put("/:id", (req, res) => {
+  // Look up for the course
+  // If does not exist, return 404
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send("Course was not found");
+
+  // Validate
+  // If invalid, return 400 - Bad request
+  const result = AuthSchema.validate(req.body);
+  if (result.error)
+    return res.status(400).send(result.error.details[0].message);
+
+  //Update course
+
+  course.name = req.body.name;
+
+  // Return the updated course
+  res.send(course);
+});
+
+router.delete("/:id", (req, res) => {
+  // Look for the course
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+
+  if (!course) return res.status(404).send("Course is not available");
+
+  //Delete
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+  for (let key in course)
+    res.send(`course ${key}  has been deleted successfully`);
+});
+module.exports = router;
