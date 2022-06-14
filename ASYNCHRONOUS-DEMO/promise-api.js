9db31fda313e4eb79b38e43acba0fd2da003b49e
Tony Maluki <tonymaluki@gmail.com>
2022-06-15 00:18:27 +0300

    running promises in parrallel

--- ASYNCHRONOUS-DEMO/promise-api.js
+++ ASYNCHRONOUS-DEMO/promise-api.js
@@ -1,5 +1,21 @@
-const p = Promise.resolve({ id: 1 });
-p.then((result) => console.log(result));
+// const p = Promise.resolve({ id: 1 });
+// p.then((result) => console.log(result));
 
-const q = Promise.reject(new Error("Reason for rejection..."));
-q.catch((error) => console.log(error));
+// const q = Promise.reject(new Error("Reason for rejection..."));
+// q.catch((error) => console.log(error));
+
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("Asyc operation 1....");
+    reject(new Error("Because something Failed..."));
+  }, 2000);
+});
+const p2 = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log("Asyc operation 2 ....");
+    resolve(2);
+  }, 2000);
+});
+Promise.race([p1, p2])
+  .then((result) => console.log(result))
+  .catch((err) => console.log("Error", err.message));
