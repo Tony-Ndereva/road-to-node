2ee03bc4840c7646c10dc40b709086ab2a24cbb7
Tony Maluki <tonymaluki@gmail.com>
2022-06-16 11:58:06 +0300

    playing with mongo db

--- MONGO-DEMO/connect.js
+++ MONGO-DEMO/connect.js
@@ -1,5 +1,14 @@
+const { date, boolean } = require("joi");
 const mongoose = require("mongoose");
 mongoose
   .connect("mongodb://localhost/playground")
   .then(() => console.log("connected to MongoDB..."))
   .catch((err) => console.log("Could not connect to MongoDB", err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: date, default: date.now },
+  isPublished: boolean,
+});
