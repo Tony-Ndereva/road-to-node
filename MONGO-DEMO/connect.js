f69d2bc4330396b7c4ba1adb841c74b0a6bb046a
Tony Maluki <tonymaluki@gmail.com>
2022-06-17 22:11:56 +0300

    Querying document files in Mongo-db

--- MONGO-DEMO/connect.js
+++ MONGO-DEMO/connect.js
@@ -1,14 +1,35 @@
-const { date, boolean } = require("joi");
 const mongoose = require("mongoose");
 mongoose
   .connect("mongodb://localhost/playground")
-  .then(() => console.log("connected to MongoDB..."))
-  .catch((err) => console.log("Could not connect to MongoDB", err));
+  .then(() => console.log("Connected to database"))
+  .catch((err) => console.error("Failed to connect to database", err));
 
+// Creating a schema for our courseDatabase
 const courseSchema = new mongoose.Schema({
   name: String,
   author: String,
   tags: [String],
-  date: { type: date, default: date.now },
-  isPublished: boolean,
+  date: { type: Date, default: Date.now },
 });
+
+const Course = mongoose.model("Course", courseSchema);
+
+async function createCourse() {
+  const course = new Course({
+    name: "Angular Course",
+    author: "Tony",
+    tags: ["Angular", "front-end"],
+    isPublished: true,
+  });
+  const result = await course.save();
+  console.log(result);
+}
+
+async function getCourses() {
+  const courses = await Course.find({ author: "Tony", isPublished: true })
+    .limit(10)
+    .sort({ name: 1 })
+    .select({ name: 1, tags: 1 });
+  console.log(courses);
+}
+getCourses();
