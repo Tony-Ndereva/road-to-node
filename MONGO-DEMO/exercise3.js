5adef3323f30d133c6df21a652a6f64127574186
Tony Maluki <tonymaluki@gmail.com>
2022-07-16 21:26:54 +0300

    Querying and updating MONGOdb DOCS

--- MONGO-DEMO/exercise3.js
+++ MONGO-DEMO/exercise3.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 mongoose
   .connect("mongodb://localhost/mongo-exercises")
   .then(() => console.log("Connected to database"))
-  .catch((err) => console.log("Failed to connect to the database", err));
+  .catch((err) => console.error("Failed to connect to the database", err));
 
 const courseSchema = new mongoose.Schema({
+  _id: String,
   tags: [String],
+  author: String,
   date: { type: Date, default: Date.now },
   name: String,
   isPublished: Boolean,
@@ -13,17 +15,33 @@ const courseSchema = new mongoose.Schema({
   version: Number,
 });
 
-const Course = mongoose.model("Courses", courseSchema);
+const Course = mongoose.model("courses", courseSchema);
 
-async function getCourses() {
-  return await Course.find({ isPublished: true }).or([
-    { price: { $gte: 15 } },
-    { name: /.*by.*/i },
-  ]);
-}
-async function run() {
-  const courses = await getCourses();
+// async function getCourses() {
+//   return await Course.find({ isPublished: true }).or([
+//     { price: { $gte: 15 } },
+//     { name: /.*by.*/i },
+//   ]);
+// }
+// async function run() {
+//   const courses = await getCourses();
 
-  console.log(courses);
+//   console.log(courses);
+// }
+// async function updateCourse(id) {
+//   const course = await Course.findById(id);
+//   if (!course) return;
+//   course.set({
+//     isPublished: false,
+//     author: "Another author",
+//     price: 25,
+//   });
+//   const result = await course.save();
+//   console.log(result);
+// }
+// updateCourse("5a68ff090c553064a218a547");
+async function updateCourse(){
+  const course = await Course.update({_id : id},{
+    
+  } )
 }
-run();
