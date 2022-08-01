d9b5a8dd2807c8725ddb6e994a3a1d37bb08d29a
Tony Maluki <tonymaluki@gmail.com>
2022-08-02 00:44:08 +0300

    working on async mongoose validators

--- MONGO-DEMO/validation.js
+++ MONGO-DEMO/validation.js
@@ -1,33 +1,76 @@
 const mongoose = require("mongoose");
 mongoose
   .connect("mongodb://localhost/mongo-exercises")
-  .then(() => console.log("connected to the datatbase"))
+  .then(() => console.log("Connected to the database"))
   .catch((err) => console.error("Failed to connect to the database", err));
 
 const courseSchema = new mongoose.Schema({
-  _id: String,
-  tags: [String],
-  author: String,
-  date: { type: Date, default: Date.now },
-  name: { type: String, required: true },
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: ["skills", "rightstick", "fifa"],
+    lowercase: true,
+    trim: true,
+  },
+  price: {
+    min: 10,
+    max: 200,
+    get: (v) => Math.round(v),
+    set: (v) => Math.round(v),
+    type: Number,
+    required: function () {
+      return this.isPublished;
+    },
+  },
   isPublished: Boolean,
-  price: Number,
-  version: Number,
+  date: { type: Date, default: Date.now },
+  tags: {
+    type: Array,
+    validate: {
+      isAsync: true,
+      validator: function (v) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            const result = v && v.length > 0;
+            resolve(result);
+          }, 1000);
+        });
+      },
+      message: "A course should have at least one tag",
+    },
+  },
+  version: Number, // 0793740785
 });
+
 const Course = mongoose.model("courses", courseSchema);
+
 async function createCourse() {
   const course = new Course({
-    // author: "Mosh",
-    tags: ["sweetCourse"],
+    name: "Fifa 22 Skill moves",
+    author: "Tekkz",
+    category: "Skills",
+    tags: ["skillmoves"],
+    price: 52.8,
     isPublished: true,
-    price: 5523,
   });
 
   try {
     const result = await course.save();
     console.log(result);
-  } catch (error) {
-    console.log(error.message);
-  }  
+  } catch (ex) {
+    console.log(ex.message);
+  }
 }
-createCourse();
+//createCourse();
+async function getCourse(id) {
+  const course = await Course.findById(id);
+  console.log(course.price)
+}
+
+getCourse("62e8468540067f39142afe8d");
