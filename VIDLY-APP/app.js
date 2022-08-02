3fba6381541928ce2c6d05a25f466285e2ae1f77
Tony Maluki <tonymaluki@gmail.com>
2022-08-02 19:02:13 +0300

    exported my customer route to app.js

--- VIDLY-APP/app.js
+++ VIDLY-APP/app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
-const genre = require("./routes/genres");
+const genres = require("./routes/genres");
+const customers = require("./routes/customers");
 
 // Since we are not working with a database,
 // (cont..) we create an empty array to store our data
@@ -12,7 +13,8 @@ mongoose
   .then(() => console.log("Connected to the database"))
   .catch((err) => console.error("Failed to connect to the database", err));
 
-app.use("/api/genres", genre);
+app.use("/api/customers", customers);
+app.use("/api/genres", genres);
 const port = 5000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
