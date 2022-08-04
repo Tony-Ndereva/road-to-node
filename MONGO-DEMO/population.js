ea4ae1a72456272b1ab20d47781f8b2508af8863
Tony Maluki <tonymaluki@gmail.com>
2022-08-04 17:43:42 +0300

    added file for working with modelling database relationships

---
+++ MONGO-DEMO/population.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/playground")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
+
+const authorSchema = new mongoose.Schema({
+  name: String,
+  bio: String,
+  website: String,
+});
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Author",
+  },
+});
+
+const Course = mongoose.model("Course", courseSchema);
+const Author = mongoose.model("Author", authorSchema);
+
+async function createAuthor(name, bio, website) {
+  const author = new Author({
+    name,
+    bio,
+    website,
+  });
+
+  const result = await author.save();
+  console.log(result);
+}
+
+async function createCourse(name, author) {
+  const course = new Course({
+    name,
+    author,
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+async function listCourses() {
+  const courses = await Course.find().populate("author").select("name author");
+  console.log(courses);
+}
+
+// createAuthor('Mosh', 'My bio', 'My Website');
+
+// createCourse("Node Course", "62ebcf5c9dd4a08d37893cd4");
+
+listCourses();
