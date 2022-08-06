ff469210f00a41335e8489ba5bfe68daffcbd7cd
Tony Maluki <tonymaluki@gmail.com>
2022-08-06 18:00:18 +0300

    imported rental API to app

--- VIDLY-APP/app.js
+++ VIDLY-APP/app.js
@@ -5,6 +5,7 @@ app.use(express.json());
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
+const rentals = require("./routes/rentals");
 
 // Since we are not working with a database,
 // (cont..) we create an empty array to store our data
@@ -17,6 +18,7 @@ mongoose
 app.use("/api/customers", customers);
 app.use("/api/genres", genres);
 app.use("/api/movies", movies);
+app.use("/api/rentals", rentals);
 const port = 5000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
