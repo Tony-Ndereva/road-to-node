020877b2c9382eae4084e2f437991316ac44bd95
Tony Maluki <tonymaluki@gmail.com>
2022-08-08 01:42:42 +0300

    loaded the users route to app.js

--- VIDLY-APP/app.js
+++ VIDLY-APP/app.js
@@ -6,7 +6,7 @@ const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
-
+const users = require("./routes/users");
 // Since we are not working with a database,
 // (cont..) we create an empty array to store our data
 
@@ -19,6 +19,7 @@ app.use("/api/customers", customers);
 app.use("/api/genres", genres);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
+app.use("/api/users", users);
 const port = 5000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
