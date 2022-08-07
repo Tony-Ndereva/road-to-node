6ecbb4fd5cb7c1f8d690138b3efb1ba32d525f4b
Tony Maluki <tonymaluki@gmail.com>
2022-08-08 00:07:55 +0300

    added joi id validations

--- EXPRESS-DEMO/validate_schema.js
+++ EXPRESS-DEMO/validate_schema.js
@@ -1,4 +1,5 @@
 const Joi = require("joi");
+Joi.objectId = require('joi-objectid')(Joi)
 
 const AuthSchema = Joi.object({
   name: Joi.string().min(3).lowercase().required(),
@@ -16,14 +17,15 @@ const customer_vidlySchema = Joi.object({
 
 const movie_vidlySchema = Joi.object({
   title: Joi.string().min(5).max(50).required(),
-  genreId: Joi.string().required(),
+  genreId: Joi.objectId().required(),
   numberInStock: Joi.number().min(0).required(),
   dailyRentalRate: Joi.number().min(0).required(),
 });
 const rental_vidlySchema = Joi.object({
-  customerId: Joi.string().required(),
+  customerId: Joi.objectId().required(),
   movieId: Joi.string().required(),
 });
+
 module.exports = {
   AuthSchema,
   genre_VidlySchema,
