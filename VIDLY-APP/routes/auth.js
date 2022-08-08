b726cbe732a9511a78666239782700991adcaa42
Tony Maluki <tonymaluki@gmail.com>
2022-08-08 15:51:46 +0300

    created a login API to authenticate existing users

---
+++ VIDLY-APP/routes/auth.js
@@ -0,0 +1,24 @@
+const { Router } = require("express");
+const express = require("express");
+const mongoose = require("mongoose");
+const route = express.Router();
+const { auth_vidlySchema } = require("../../EXPRESS-DEMO/validate_schema");
+const { User } = require("../models/user");
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
+
+route.post("/", async (req, res) => {
+  const results = auth_vidlySchema.validate(req.body);
+  if (results.error)
+    return res.status(400).send(results.error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid e-mail or password");
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send("Invalid e-mail or password");
+  res.send(true);
+
+});
+
+module.exports = route;
