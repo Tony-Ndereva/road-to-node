9d8f40e06b4edcce9cd73cfbc2062eab3e4a1b20
Tony Maluki <tonymaluki@gmail.com>
2022-08-08 14:43:47 +0300

    implemented hashing user's passwords

--- VIDLY-APP/routes/users.js
+++ VIDLY-APP/routes/users.js
@@ -5,6 +5,7 @@ const route = express.Router();
 const { user_vidlySchema } = require("../../EXPRESS-DEMO/validate_schema");
 const { User } = require("../models/user");
 const _ = require("lodash");
+const bcrypt = require("bcrypt");
 
 route.post("/", async (req, res) => {
   const results = user_vidlySchema.validate(req.body);
@@ -14,12 +15,9 @@ route.post("/", async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User already registered");
 
-  user = new User(
-    // name: req.body.name,
-    // email: req.body.email,
-    // password: req.body.password,
-    _.pick(req.body, ["name", "email", "password"])
-  );
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
   res.send(_.pick(user, ["_id", "name", "email"]));
