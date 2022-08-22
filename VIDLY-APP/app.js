62b5b1d8f9a222782e81772d3a8ef2ff6e848b20
Tony Maluki <tonymaluki@gmail.com>
2022-08-23 02:47:37 +0300

    refactored code

--- VIDLY-APP/app.js
+++ VIDLY-APP/app.js
@@ -1,59 +1,23 @@
 const express = require("express");
-require("express-async-errors");
-require("winston-mongodb");
-const winston = require("winston");
-const mongoose = require("mongoose");
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
-const config = require("config");
-const _ = require("lodash");
-const error = require("./middleware/error");
 
-// process.on("uncaughtException", (ex) => {
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+const config = require("config");
+require("./startup/logging");
+require("./startup/routes");
+require("./startup/connect");
+// process.on("uncaughtE xception", (ex) => {
 //   console.log("We got an uncaught exception");
 //   winston.error(ex.message, ex);
 // });
-winston.handleExceptions(
-  new winston.transports.File({ filename: "uncaughtExceptions.log" })
-);
 
-process.on("unhandledRejection", (ex) => {
-  throw ex;
-});
-winston.add(winston.transports.File, { filename: "logfile.log" });
-winston.add(winston.transports.MongoDB, {
-  db: "mongodb://localhost/vidly",
-  level: "error",
-});
-
-// throw new Error("Something failed during startup");
-
+throw new Error("Something failed during startup");
 
 if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined");
   process.exit(1);
 }
 
-mongoose
-  .connect("mongodb://localhost/vidly")
-  .then(() => console.log("Connected to the database"))
-  .catch((err) => console.error("Failed to connect to the database", err));
-
-app.use("/api/customers", customers);
-app.use("/api/genres", genres);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-
-app.use(error);
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
