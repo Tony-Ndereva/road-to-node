bb2351f884f0fc2265bf010a4ee55fae675d608c
Tony Maluki <tonymaluki@gmail.com>
2022-08-23 15:17:40 +0300

    updated privatekeys err messagw

--- VIDLY-APP/startup/config.js
+++ VIDLY-APP/startup/config.js
@@ -1,6 +1,8 @@
 const config = require("config");
 module.exports = function () {
   if (!config.get("jwtPrivateKey")) {
-    throw new Error("FATAL ERROR: jwtPrivateKey is not defined");
+    throw new Error(
+      "FATAL ERROR: jwtPrivateKey is not defined, on terminal run this command: set vidly_jwtPrivatekey=mySecretKey"
+    );
   }
 };
