40009677cd5f48ef27741a9ea29334de8207f4a3
Tony Maluki <tonymaluki@gmail.com>
2022-08-23 11:24:37 +0300

    cleaned up app.js code

--- VIDLY-APP/app.js
+++ VIDLY-APP/app.js
@@ -1,24 +1,15 @@
 const express = require("express");
+const winston = require("winston");
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-const config = require("config");
-require("./startup/logging");
-require("./startup/routes");
+
+require("./startup/logging")();
+require("./startup/routes")(app);
 require("./startup/connect");
-// process.on("uncaughtE xception", (ex) => {
-//   console.log("We got an uncaught exception");
-//   winston.error(ex.message, ex);
-// });
-
-// throw new Error("Something failed during startup");
-
-if (!config.get("jwtPrivateKey")) {
-  console.error("FATAL ERROR: jwtPrivateKey is not defined");
-  process.exit(1);
-}
+require("./startup/config")();
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  winston.info(`Listening on port ${port}`);
 });
