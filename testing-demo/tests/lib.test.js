3956915df87e9e0e7b89e032e0e001e7cd687165
Tony Maluki <tonymaluki@gmail.com>
2022-08-28 17:08:49 +0300

    tests with jest

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,4 +1,16 @@
 const lib = require("../lib");
+const db = require("../db");
+describe("applyDiscount", () => {
+  it("should apply 10% discount if customer has more than 10 points", () => {
+    db.getCustomerSync = function (customerId) {
+      console.log("Fake reading customer ....");
+      return { id: customerId, points: 20 };
+    };
+    const order = { customerId: 1, totalPrice: 10 };
+    lib.applyDiscount(order);
+    expect(order.totalPrice).toBe(9);
+  });
+});
 
 // describe("absolute", () => {
 //   it(" should return a positive number if input is positive ", () => {
@@ -25,15 +37,45 @@ const lib = require("../lib");
 //   });
 // });
 
-describe("getCurrencies", () => {
-  it("Should return supported currencies", () => {
-    const result = lib.getCurrencies();
-    expect(result).toContain("USD");
-    expect(result).toContain("AUD");
-    expect(result).toContain("EUR");
+// describe("getCurrencies", () => {
+//   it("Should return supported currencies", () => {
+//     const result = lib.getCurrencies();
+//     // expect(result).toContain("USD");
+//     // expect(result).toContain("AUD");
+//     // expect(result).toContain("EUR");
 
+//     //ideal
+//     expect(result).toEqual(expect.arrayContaining(["EUR", "USD", "AUD"]));
+//   });
+// });
 
-    // 
-    expect
-  });
-});
+// describe("getProduct", () => {
+//   it("should return the product with the given id", () => {
+//     const result = lib.getProduct(1);
+//     expect(result).toEqual({ id: 1, price: 10 });
+//   });
+// });
+
+// describe("registerUSer", () => {
+//   it("should throw if username is falsy", () => {
+//     //Null
+//     //Undefined
+//     //NaN
+//     //''
+//     // 0
+//     // false
+//     const args = [null, undefined, NaN, "", 0, false];
+
+//     args.map((a) => {
+//       expect(() => {
+//         lib.registerUser(a);
+//       }).toThrow();
+//     });
+//   });
+
+//   it("should return a user object if value username is passed", () => {
+//     const result = lib.registerUser("Tony");
+//     expect(result).toMatchObject({ username: "Tony" });
+//     expect(result.id).toBeGreaterThan(0);
+//   });
+// });
