109aff10ce787aa4c92e65314bd53041fa51f6e1
Tony Maluki <tonymaluki@gmail.com>
2022-08-28 23:47:07 +0300

    migrated connect string to config

--- VIDLY-APP/startup/connect.js
+++ VIDLY-APP/startup/connect.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
 const winston = require("winston");
-module.exports = mongoose
-  .connect("mongodb://localhost/vidly")
-  .then(() => winston.info("Connected to the database"));
+const config = require("config");
+module.exports = function () {
+  const db = config.get("db");
+  mongoose
+    .connect(config.get("db"))
+    .then(() => winston.info(`Connected to the ${db}...`));
+};
