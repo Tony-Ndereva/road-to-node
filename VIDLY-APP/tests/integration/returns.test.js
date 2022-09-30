6bdb77e5fb67b43730d6e3e7c08e3684ab87eb57
Tony Maluki <tonymaluki@gmail.com>
2022-09-30 20:02:43 +0300

    written more test driven code

--- VIDLY-APP/tests/integration/returns.test.js
+++ VIDLY-APP/tests/integration/returns.test.js
@@ -58,4 +58,20 @@ describe("/api/returns", () => {
 
     expect(res.status).toBe(400);
   });
+
+  it("should return error 404 if no rental found for this customer/movie", async () => {
+    await Rental.remove({});
+    const res = await exec();
+    expect(res.status).toBe(404);
+  });
+  it("should return error 400 if return is already processed", async () => {
+    rental.dateReturned = new Date();
+    await rental.save();
+    const res = await exec();
+    expect(res.status).toBe(400);
+  });
+  it("should return code 200 if valid request", async () => {
+    const res = await exec();
+    expect(res.status).toBe(200)
+  });
 });
