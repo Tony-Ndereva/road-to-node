1cfcbaaf9a04f20ff3f1c3a19fe34ad82a553e8a
Tony Maluki <tonymaluki@gmail.com>
2022-09-30 17:08:17 +0300

    validating returns test

--- VIDLY-APP/routes/returns.js
+++ VIDLY-APP/routes/returns.js
@@ -2,6 +2,9 @@ const express = require("express");
 const route = express.Router();
 
 route.post("/", (req, res) => {
+  if (!req.body.customerId)
+    return res.status(400).send("customerId is not provided");
+  if (!req.body.movieId) return res.status(400).send("movieId is not provided");
   res.status(401).send("Unauthorised");
 });
 module.exports = route;
