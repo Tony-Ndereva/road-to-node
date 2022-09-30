c3433bb664e4ec6bf217d23f195f7c87ea95390c
Tony Maluki <tonymaluki@gmail.com>
2022-09-30 17:09:31 +0300

    testing customerId and movieId

--- VIDLY-APP/tests/integration/returns.test.js
+++ VIDLY-APP/tests/integration/returns.test.js
@@ -1,4 +1,5 @@
 const { Rental } = require("../../models/rental");
+const { User } = require("../../models/user");
 const mongoose = require("mongoose");
 const request = require("supertest");
 
@@ -31,10 +32,30 @@ describe("/api/returns", () => {
     await Rental.remove({});
   });
   it("should return 401 if client is not logged in", async () => {
-    const res = request(server)
+    const res = await request(server)
       .post("/api/returns")
       .send({ customerId, movieId });
 
     expect(res.status).toBe(401);
+  });
+
+  it("should return error 400 if custtomerId is not provided", async () => {
+    const token = new User().generateAuthToken();
+    const res = await request(server)
+      .post("/api/returns")
+      .set("x-auth-token", token)
+      .send({ movieId });
+
+    expect(res.status).toBe(400);
+  });
+
+  it("should return error 400 if movieId is not provided", async () => {
+    const token = new User().generateAuthToken();
+    const res = await request(server)
+      .post("/api/returns")
+      .set("x-auth-token", token)
+      .send({ customerId });
+
+    expect(res.status).toBe(400);
   });
 });
