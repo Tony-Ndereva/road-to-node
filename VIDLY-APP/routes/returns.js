f6f20a040cb40e0ab6ebe8d14b1fe615adb27dec
Tony Maluki <tonymaluki@gmail.com>
2022-09-30 20:02:55 +0300

    more tests

--- VIDLY-APP/routes/returns.js
+++ VIDLY-APP/routes/returns.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const { Rental } = require("../models/rental");
 const route = express.Router();
-
-route.post("/", (req, res) => {
+const auth = require('../middleware/auth')
+route.post("/",auth, async (req, res) => {
   if (!req.body.customerId)
     return res.status(400).send("customerId is not provided");
   if (!req.body.movieId) return res.status(400).send("movieId is not provided");
-  res.status(401).send("Unauthorised");
+
+  const rental = await Rental.findOne({
+    "customer._id": req.body.customerId,
+    "movie._id": req.body.movieId,
+  });
+  if (!rental) return res.status(404).send("no rental found for this customer");
+
+  if (rental.dateReturned)
+    return res.status(400).send("Return already processed");
+
+  return res.status(200).send("Rental found");
+
 });
+
 module.exports = route;
