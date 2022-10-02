e706bcc058cb2cb1e8a96253ae71932c9a8396b7
Tony Maluki <tonymaluki@gmail.com>
2022-10-03 02:30:04 +0300

    passed helmet and compression to production code

--- VIDLY-APP/app.js
+++ VIDLY-APP/app.js
@@ -8,6 +8,7 @@ require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/connect")();
 require("./startup/config")();
+require("./startup/prod")(app);
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
